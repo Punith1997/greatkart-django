248979a595c55e1415465b29d4a807a0b819fee2
Your Name <you@example.com>
2021-09-13 16:01:19 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,3 +133,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'punithdjango@gmail.com'
+EMAIL_HOST_PASSWORD = '9980@abcd'
+EMAIL_USE_TLS=True
